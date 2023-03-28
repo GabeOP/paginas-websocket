ccfa02484d48e86bd70ead9c87fe6ce6df14b04b
GabeOP <gabrieloliveirap1@hotmail.com>
2023-03-28 13:33:38 -0300

    feat: formulario para criacao de usuario

---
+++ login/login.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("formCadastro")
+
+form.addEventListener('submit', (e)=>{
+  // const name = document.getElementById('nicknameInput').value;
+  // const password = document.getElementById('senhaInput').value;
+
+  // const dados = {name, password}
+})
