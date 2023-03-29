9cc6b87f75a22bf29dbfa3015c8a009cf46922d2
GabeOP <gabrieloliveirap1@hotmail.com>
2023-03-29 13:24:45 -0300

    exclui arquivo

--- login/login.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById("formCadastro")
-
-form.addEventListener('submit', (e)=>{
-  // const name = document.getElementById('nicknameInput').value;
-  // const password = document.getElementById('senhaInput').value;
-
-  // const dados = {name, password}
-})
