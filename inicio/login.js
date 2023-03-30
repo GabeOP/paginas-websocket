aaee3f23c455224a4d96bab92030c9f021781979
GabeOP <gabrieloliveirap1@hotmail.com>
2023-03-30 16:00:13 -0300

    feat: verificacao login usuario

--- inicio/login.js
+++ inicio/login.js
@@ -2,32 +2,37 @@ const formLogin = document.getElementById("formLogin");
 const nicknameLogin = document.getElementById("nicknameLogin");
 const nicknameSenha = document.getElementById("nicknameSenha");
 
-formLogin.addEventListener("submit", logarUsuario)
+formLogin.addEventListener("submit", logarUsuario);
 
-function logarUsuario(e){
+function logarUsuario(e) {
   e.preventDefault();
+
+  if(!nicknameLogin.value|| !nicknameSenha.value){
+    return alert("Preencha todos os campos.")
+  }
+
 
   const data = {
     name: nicknameLogin.value,
-    password: nicknameSenha.value 
-  }
+    password: nicknameSenha.value,
+  };
 
   fetch("http://localhost:3000/login", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   })
-  .then(res=> {
-    if(res.status === 200){
-      alert("Login realizado com sucesso!");
-      localStorage.setItem("nickname", nicknameLogin.value)
-      // window.location.replace("http://127.0.0.1:5500/sala1/sala1.html")
-      
-    }else{
-      alert("Usuário não encontrado!")
-    }
-  })
-  .catch((err) => console.log(err))
+    .then((res) => {
+      if (res.status === 200) {
+        alert("Login realizado com sucesso!");
+        localStorage.setItem("nickname", nicknameLogin.value);
+        window.location.replace("http://127.0.0.1:5500/sala1/sala1.html")
+      } else if(res.status === 404){
+        alert("Usuário não encontrado!")
+      }else if(res.status === 422){
+        alert("Senha incorreta.");
+    }})
+    .catch((err) => console.log(err));
 }
