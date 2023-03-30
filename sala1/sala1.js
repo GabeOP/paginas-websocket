a2cc6e52c82658c6ffbc1b9167c66a1808a3cecb
GabeOP <gabrieloliveirap1@hotmail.com>
2023-03-30 16:01:32 -0300

    att

--- sala1/sala1.js
+++ sala1/sala1.js
@@ -1,36 +1,28 @@
 // Conexão com o servidor Socket.io
 const socket = io("http://localhost:3000/sala1");
 
-
+//=========Parte que cuida das mensagens=========//
 const form = document.getElementById("form");
 const input = document.getElementById("input");
-const mensagens = document.getElementById("messages")
+const mensagens = document.getElementById("messages");
 
-form.addEventListener('submit', (e)=>{
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if(input.value){
-    socket.emit('chat message', input.value);
-    input.value = '';
+  if (input.value) {
+    socket.emit("chat message", input.value);
+    input.value = "";
   }
-})
-
-
-// Escute o evento de conexão
-socket.on("connect", () => {
-  const engine = socket.io.engine;
-  console.log(engine);
-  console.log("Conectado na sala 1");
-  
-  socket.on('chat message', (msg)=>{
-    const item = document.createElement('li');
-    item.textContent = msg;
-    console.log(msg)
-    mensagens.appendChild(item);
-    window.scrollTo(0, document.body.scrollHeight);
-  })
 });
 
-
+socket.on("connect", () => {
+  socket.on("chat message", (msg) => {
+    const item = document.createElement("li");
+    const teste = localStorage.getItem("nickname");
+    item.textContent = `${teste}: ${msg}`;
+    mensagens.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+  });
+});
 
 // Escute o evento de desconexão
 socket.on("disconnect", () => {
